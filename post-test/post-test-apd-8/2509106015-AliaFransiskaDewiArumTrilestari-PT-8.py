a8be8cc51ee4a11dc44057affa7189f8035a50e7
Alia <aliafransiska30@gmail.com>
2025-11-03 18:51:06 +0700

    posttestapd

--- post-test/post-test-apd-8/2509106015-AliaFransiskaDewiArumTrilestari-PT-8.py
+++ post-test/post-test-apd-8/2509106015-AliaFransiskaDewiArumTrilestari-PT-8.py
@@ -5,18 +5,17 @@ from login import login
 from read import tampilkan_alat
 from update import ubah_status
 from login import pengguna
-from warna import judul, berhasil, gagal, enter, m, gumi
 
 login_user = ""
 
 while True:
     clear()
-    print(judul + "=== SISTEM PENGELOLA ALAT BERAT ===")
-    print(m + "1. Login")
-    print(m + "2. Register")
-    print(m + "3. Keluar")
+    print("=== SISTEM PENGELOLA ALAT BERAT ===")
+    print("1. Login")
+    print("2. Register")
+    print("3. Keluar")
 
-    menu = input(judul + "Pilih menu (1-3): ")
+    menu = input("Pilih menu (1-3): ")
 
     if menu == "1":
         login_user = login() 
@@ -29,67 +28,67 @@ while True:
         if akses == "admin":
             while True:
                 clear()
-                print(judul + f"=== MENU ADMIN ({login_user}) ===")
-                print(m + "1. Tambah alat baru")
-                print(m + "2. Lihat semua alat")
-                print(m + "3. Ubah status alat")
-                print(m + "4. Hapus alat")
-                print(m + "5. Keluar")
+                print(f"=== MENU ADMIN ({login_user}) ===")
+                print("1. Tambah alat baru")
+                print("2. Lihat semua alat")
+                print("3. Ubah status alat")
+                print("4. Hapus alat")
+                print("5. Keluar")
 
-                pilihan = input(gumi + "Pilih menu (1-5): ")
+                pilihan = input( "Pilih menu (1-5): ")
                 if pilihan == "1":
                     tambah_alat()
                 elif pilihan == "2":
                     tampilkan_alat()
                 elif pilihan == "3":
                     clear()
-                    alat = input(gumi + "Masukkan nama alat yang mau diubah: ").strip()
-                    status_baru = input(gumi + "Masukkan status baru (Siap Pakai/Rusak/Dalam Perbaikan) : ").strip()
+                    alat = input( "Masukkan nama alat yang mau diubah: ").strip()
+                    status_baru = input( "Masukkan status baru (Siap Pakai/Rusak/Dalam Perbaikan) : ").strip()
                     ubah_status(alat, status_baru)
                 elif pilihan == "4":
                     hapus_alat()
                 elif pilihan == "5":
                     break
                 else:
-                    print(gagal + "Gunakan nomor menu yang sudah disediakan!")
-                input(enter + "Tekan Enter untuk melanjutkan...")
+                    print("Gunakan nomor menu yang sudah disediakan!")
+                input("Tekan Enter untuk melanjutkan...")
         # === MENU USER ===
         else:
             while True:
                 clear()
-                print(judul + f"=== MENU USER ({login_user}) ===")
+                print(f"=== MENU USER ({login_user}) ===")
                 print(menu + "1. Lihat alat berat")
                 print(menu + "2. Keluar")
 
-                pilihan = input(gumi + "Pilih menu (1-2): ")
+                pilihan = input( "Pilih menu (1-2): ")
 
                 if pilihan == "1":
                     tampilkan_alat()
                 elif pilihan == "2":
                     break
                 else:
-                    print(gagal + "Gunakan menu 1-3!")
-                input(enter + "Tekan Enter untuk melanjutkan...")
+                    print("Gunakan menu 1-3!")
+                input("Tekan Enter untuk melanjutkan...")
     # === REGISTER ===
     elif menu == "2":
         clear()
-        print(judul + "=== REGISTER AKUN BARU ===")
-        username = input(gumi + "Masukkan username baru: ").strip()
-        password = input(gumi + "Masukkan password baru: ").strip()
+        print("=== REGISTER AKUN BARU ===")
+        username = input( "Masukkan username baru: ").strip()
+        password = input( "Masukkan password baru: ").strip()
 
         if username in pengguna:
-            print(gagal + "Username sudah terdaftar!")
+            print("Username sudah terdaftar!")
         else:
             pengguna[username] = {"password": password, "akses": "user"}
             print(f"Akun '{username}' berhasil dibuat sebagai user!")
-        input(enter + "Tekan Enter untuk melanjutkan...")
+        input("Tekan Enter untuk melanjutkan...")
 
     elif menu == "3":
         clear()
-        print(judul + "Program berhenti.")
+        print("Program berhenti.")
         break
 
     else:
         clear()
-        print(gagal + "Pilihan menu tidak sesuai! Gunakan nomor menu yang sudah disediakan!")
-        input(enter + "Tekan Enter untuk melanjutkan...")
+        print("Pilihan menu tidak sesuai! Gunakan nomor menu yang sudah disediakan!")
+        input("Tekan Enter untuk melanjutkan...")
--- post-test/post-test-apd-8/warna.py
+++
@@ -1,9 +0,0 @@
-from colorama import Fore, Style, init
-init(autoreset=True)
-
-judul = Fore.LIGHTBLUE_EX + Style.BRIGHT
-enter = Fore.LIGHTCYAN_EX + Style.NORMAL
-berhasil = Fore.LIGHTGREEN_EX + Style.NORMAL
-gagal = Fore.LIGHTRED_EX + Style.NORMAL
-m = Fore.LIGHTMAGENTA_EX + Style.NORMAL
-gumi = Fore.YELLOW + Style.BRIGHT
